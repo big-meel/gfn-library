77c8af3e4c16c9c7ad8cfe51662588973beb4bad
big-meel <big.meel@zoho.com>
2020-11-25 12:19:13 -0500

    Add search params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,12 @@
 class GamesController < ApplicationController
   def index
-    # @games = Game.load_all_games
+    @games = Game.all
   end
+
+  private
+
+  def search_params
+    params.permit!
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,9 @@ class Game < ApplicationRecord
 
   # has_one_attached :cover
 
-  
+  def self.search(keyword)
+    
+  end
 
 end
 
