f801977f259c292a1d0efaaac9579056d8394f1e
big-meel <big.meel@zoho.com>
2020-12-01 00:54:34 -0500

    Add search functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,13 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    # @games = Game.all
+    @games = Game.search(params[:search])
   end
 
   private
 
   def search_params
-    params.permit!
+    params.permit(:search)
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,12 @@ class Game < ApplicationRecord
   # has_one_attached :cover
 
   def self.search(keyword)
-    
+    if keyword
+      keyword = keyword.downcase
+      return distinct.where("LOWER(games.title) LIKE LOWER(?)", "%#{keyword}%")
+    else
+      return all
+    end
   end
 
 end
