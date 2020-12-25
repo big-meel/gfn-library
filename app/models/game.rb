088f0083b0243a337f43c180d14233a808e00e54
big-meel <big.meel@zoho.com>
2020-12-25 15:52:58 -0500

    Add filter scopes

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,9 @@ class Game < ApplicationRecord
   # has_one_attached :cover
   serialize :genres
 
+  scope :filter_by_title, -> (title) { where(title: title.parameterize.underscore) unless title.nil? || title.blank? }
+  scope :filter_by_store, -> (online_store) { where(store: online_store.parameterize.underscore ) unless online_store.nil? || online_store.blank? }
+
   def self.search(keyword)
     if keyword
       keyword = keyword.downcase
