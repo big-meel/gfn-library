ee96b14642aefc1942381eff5b15ec68a94b47df
big-meel <big.meel@zoho.com>
2020-12-31 12:04:11 -0500

    move genre filter to helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,13 @@
 module GamesHelper
+  require 'set'
+
+
+  def filter_by_genre(records, genre)
+    if genre
+      return records.select { |game| Set(genre).subset?(Set(game.genres)) }
+    else
+      return records.all
+    end
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,11 @@
 class Game < ApplicationRecord
-
+  
   # has_one_attached :cover
   serialize :genres
-
+  
   # scope :filter_by_genre, -> (genre) { where("? IN genre", genre) }
-  scope :filter_by_store, -> (online_store) { where(store: online_store.parameterize.underscore ) unless online_store.nil? || online_store.blank? }
-
+  scope :filter_by_store, -> (online_store) { where(store: online_store ) unless online_store.nil? || online_store.blank? }
+  
   def self.search(keyword)
     if keyword
       keyword = keyword.downcase
@@ -14,6 +14,14 @@ class Game < ApplicationRecord
       return all
     end
   end
+  
+  # def self.filter_by_genre(records, genre)
+  #   if genre
+  #     return records.select { |game| Set(genre).subset?(Set(game.genres)) }
+  #   else
+  #     return records.all
+  #   end
+  # end
 
 end
 
