c65a55d65aa6f5da4a49b043b1cc107c9b978f0c
big-meel <big.meel@zoho.com>
2022-05-02 23:50:57 -0500

    Update to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_063314) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_04_063314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2022_01_04_063314) do
   end
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "title"
     t.string "sort_name"
     t.boolean "is_fully_optimized"
